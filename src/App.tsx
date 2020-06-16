8ee4caa224f5fc51f6422c2b1d5db7a684017927
Enis Ozgit <eozgit@gmail.com>
2020-06-16 17:23:35 +0100

    Add atlaskit css reset package

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../node_modules/@atlaskit/css-reset/dist/bundle.css';
 import './App.css';
 import { withAuthenticator } from '@aws-amplify/ui-react'
 import Time from './Time';
